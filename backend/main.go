a8757ceadd3f2e357526f73817392b4e34fb9dd8
daleksprinter <b1015120@fun.ac.jp>
2019-09-07 13:08:57 +0900

    prefectures

--- backend/main.go
+++ backend/main.go
@@ -34,12 +34,25 @@ func main(){
 	dt := time.Now()
 	date := dt.Format("2006-01-02")
 
+	prefectures_kanji := [...]string{"北海道", "青森", "岩手", "宮城", "秋田", "山形", "福島", "茨城",
+								 "栃木", "群馬", "埼玉", "千葉", "東京", "神奈川", "新潟", "富山", 
+								"石川", "福井", "山梨", "長野", "岐阜", "静岡", "愛知", "三重", "滋賀", "京都", "大阪", 
+								"兵庫", "奈良", "和歌山", "鳥取", "島根", "岡山", "広島", "山口", "徳島", "香川",
+								"愛媛", "高知", "福岡", "佐賀", "長崎", "熊本", "大分", "宮崎", "鹿児島", "沖縄"}
+							}
+	prefectures_roma := [...]string{"hokkaido", "aomori", "iwate", "miyagi", "akita", "yamagata", "fukushima", "ibaraki",
+							"tochigi", "gunma", "saitama", "chiba", "tokyo", "kanagawa", "niigata", "toyama", 
+						   "ishikawa", "fukui", "yamanashi", "nagano", "gifu", "sizuoka", "aichi", "mie", "siga", "kyoto", "osaka", 
+						   "hyogo", "nara", "wakayama", "tottori", "shimane", "okayama", "hiroshima", "yamaguchi", "tokushima", "kagawa",
+						   "ehime", "kouchi", "fukuoka", "saga", "nagasaki", "kumamoto", "oita", "miyazaki", "kagoshima", "okinawa"}
+					   }
+
 	keyword := "タピオカ since:" + date
 
 	v := url.Values{}
 	v.Set("count", "100")
 	
-	
+
 
 
 //	count := 0
@@ -47,7 +60,7 @@ func main(){
 	for _ , tweet := range searchResult.Statuses {
 		fmt.Println(tweet.CreatedAt);
 	}
-
+	fmt.Println(prefectures)
 	fmt.Println(len(searchResult.Statuses))
 
 
